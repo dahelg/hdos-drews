3a945517593c1205f6dce9f5e561025f32c05b89
Helge Drews <dev@hdrews.de>
2020-10-14 22:20:19 +0200

    📦 NEW: First version

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Helge Drews - Software Engineer 🦊
+
+Hello! 👋😃 I'm Helge, a full-stack software engineer focused on web development.
+I’m currently working on Jamstack webpages built with ES6, React, Node.js, Contentful, AWS and a lot of other third party services.
+I also use Python 🐍 for webdev and for RaspberryPi fun.
+
+🕊  Twitter: https://twitter.com/helgedrews
+🐙 GitHub: https://github.com/dahelg
+🌍 Website: https://hdrews.de/
+`);
