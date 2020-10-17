8f7e8c2bca591d70acbed925d93de7a74c137ada
Helge Drews <dev@hdrews.de>
2020-10-17 17:52:50 +0200

    👌 IMPROVE: Add welcome info

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
-const clearConsole = require("clear-any-console");
 const pgkJSON = require("./package.json");
+const welcome = require("cli-welcome");
 
-// Clears the console.
-clearConsole();
+welcome({
+    title: `hdos-drews`,
+    tagLine: `Nice to meet you!`,
+    bgColor: `#3150AE`,
+    color: `#F9E3E2`,
+    bold: true,
+    clear: true,
+    version: pgkJSON.version,
+    description: pgkJSON.description,
+});
 
 console.log(`
 NAME: ${pgkJSON.name}
@@ -16,7 +24,7 @@ console.log(`
 Helge Drews - Software Engineer - Open Sourcerer 🦊
 
 Hello! 👋😃 I'm Helge, a full-stack software engineer focused on web development.
-I’m currently working on Jamstack webpages built with ES6, React, Node.js, Contentful, AWS and a lot of other third party services.
+I’m currently working on Jamstack webpages built with ES6, React, No de.js, Contentful, AWS and a lot of other third party services.
 I also use Python 🐍 for webdev and for RaspberryPi fun.
 
 🕊  Twitter: https://twitter.com/helgedrews
